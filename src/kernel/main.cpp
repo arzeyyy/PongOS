3138654e439b02e0beb9edd96ba4d530afd6ce8f
arzeyyy <gifexgifex2@gmail.com>
2023-03-15 12:14:46 -0400

    double buffering

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -8,14 +8,15 @@
 #include "idt.h"
 #include "isr.h"
 
-#define FPS 60
+#define FPS 30
+const float MS_PER_UPDATE = 1000.0f / FPS;
 
 void *__gxx_personality_v0 = 0;
 void *_Unwind_Resume = 0;
 
 void trigger_exception()
 {
-    //asm volatile("int $0x3");
+    //asm volatile("int $0x8");
     //asm volatile("int $0x0");
 
     // uint_8 a = 5;
@@ -31,8 +32,28 @@ void init_kernel()
     // Initialize IDT and IRQ
     isr_init();
     sti();
-    timer_init(10);
+    timer_init(FPS);
     init_keyboard();
+}
+
+
+
+void render()
+{
+    //clear
+    clear();
+
+    // draw
+    // draw_pixel(319, 199, 255);
+    //monitor_write("idk");
+    // Font font;
+    // font.setString("idk");
+    // font.draw();
+
+
+
+    //display
+    swap_buffers();
 }
 
 extern "C" void main()
@@ -40,23 +61,88 @@ extern "C" void main()
 
     init_kernel();
 
+    // uint_8 a = 5;
+    // uint_8 b = 0;
+    // uint_8 c = a / b;
 
-    screen_clear();
-    draw_palette_tester();
-    screen_swap();
-    //draw_pixel(0, 0, 255);
-
-    //trigger_exception();
+    trigger_exception();
 
     // Game game;
     // game.Init();
+    init_system();
+
+    // clear();
+    // draw_pixel(319, 199, 255);
+    // swap_buffers();
+    // swap_buffers();
+
+    // uint_8 array[SCREEN_SIZE];
+    // memset(array, 0, SCREEN_SIZE);
+
+    // screen_clear();
+    // swap_buffers(); // swap double buffer
+    
+
+    float delta_time = 0.0f;
+    uint_64 last_tick = timer_get();
+
 
     while (true)
     {
-        //game.Run();
-        scroll_screen();
-        //sleep(50);
+        // Delta time
+        uint_64 curr_tick = timer_get();
+        delta_time += (curr_tick - last_tick) / (float)state.freq;
+        last_tick = curr_tick;
+
+        // Update
+        while (delta_time >= MS_PER_UPDATE)
+        {
+            //rect.move(10, 10);
+            //update_game_state(MS_PER_UPDATE);
+            delta_time -= MS_PER_UPDATE;
+        }
+        // clear
+        //clear();
+
+        // draw
+        // draw_pixel(319, 199, 255);
+
+        //rect.draw();
+
+        // display
+        //swap_buffers();
+
+        render();  //clear, draw, swap
     }
+
+    // while (true)
+    // {
+    //     float delta_time = get_delta_time();
+
+    //     if (delta_time >= MS_PER_UPDATE) // draw frames at fixed timestep
+    //     {
+    //         clear();
+    //         draw_pixel(319, 199, 255);
+    //         swap_buffers();
+    //         //draw_pixel(319, 199, 255);
+      
+    //         // Rect rect;
+    //         // rect.setPosition(120, 120);
+    //         // rect.setSize(10, 10);
+    //         // rect.draw();    // calling draw pixel function
+    //         //sti();
+    //         //draw_frame();   // draw game state to buffer
+    //         //cli();
+    //         delta_time -= MS_PER_UPDATE;
+    //     }
+
+    //     //screen_clear();
+    //     //draw_palette_tester();
+    //     // draw_pixel(0, 0, 255);
+    //     //game.Run();
+    //     //scroll_screen();
+    //     //sleep(50);
+    // }
 
     // while (1)
     // {
